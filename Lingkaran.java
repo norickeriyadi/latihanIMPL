5e72db27ec3e88c90088304efb145820805b8e36
norickeriyadi <76475671+norickeriyadi@users.noreply.github.com>
2022-10-08 19:41:08 +0700

    Membuat Lingkaran.java

---
+++ Lingkaran.java
@@ -0,0 +1,21 @@
+/**
+*<h1><b>Kelas Lingkaran</b></h1>
+*kelas ini merepresentasikan Lingkaran sebagai suatu tipe data,
+*sebuah bangun datar sudut, 
+*berupa himpunan titik-titik yang berjarak sama ke sebuah titik pusat
+*Di bawah <i>filed</i> yang dimilikinya adalah jejari, yang menyatakan jarak
+*titik-titik itu ke titik pusat.
+*/
+public class Lingkaran 
+{
+  float jejari;
+  Lingkaran()
+  {
+    jejari=0;
+  }
+
+  Lingkaran(float r)
+  {
+   jejari=r;
+  }
+}
